dac672b2e183b5fc82118aa206d6e9a397e2bfc9
limjongseung <101609328+limjongseung@users.noreply.github.com>
2023-12-26 17:03:12 +0900

    product 페이지에 제목 추가

--- frontend/public/product/product.js
+++ frontend/public/product/product.js
@@ -1,7 +1,8 @@
 import { formatPrice } from "/frontend/public/common/global-function.js";
 //params가져오기
-const url = "https://port-0-elice-first-project-backend-12fhqa2llo78zgu7.sel5.cloudtype.app/";
-const url_local = "http://localhost:3000/"
+const url =
+	"https://port-0-elice-first-project-backend-12fhqa2llo78zgu7.sel5.cloudtype.app/";
+const url_local = "http://localhost:3000/";
 function getQueryParam(name) {
 	const urlSearchParams = new URLSearchParams(window.location.search);
 	return urlSearchParams.get(name);
@@ -10,7 +11,7 @@ async function renderProduct(productData) {
 	const productContainer = document.getElementById("productContainer");
 	productData.map((item) => {
 		// Create HTML elements for each product property
-    item.price = formatPrice(item.price);
+		item.price = formatPrice(item.price);
 		productContainer.innerHTML += `
         <div class="product_wrap">
         <img src="${url_local}product/imgs/${item.img}" alt="${item.name}">
@@ -21,9 +22,7 @@ async function renderProduct(productData) {
 }
 async function getProduct(productId) {
 	try {
-		const res = await fetch(
-			`${url_local}product?categoryShortId=${productId}`
-		);
+		const res = await fetch(`${url_local}product?categoryShortId=${productId}`);
 		const data = await res.json();
 		console.log(data);
 		await renderProduct(data);
@@ -42,9 +41,31 @@ document.addEventListener("DOMContentLoaded", function () {
 	getProduct(productId);
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+	const shortId = getQueryParam("id");
+
+	async function getCategory() {
+		try {
+			const data = await fetch(`${url_local}category`);
+			const res = await data.json();
+			// Corrected the comparison to use === for strict equality
+			const filteredCategory = res.filter((item) => item.shortId === shortId);
+			const productName = document.getElementById("product_name");
+			productName.innerText = filteredCategory[0].name;
+
+		} catch (e) {
+			console.log(e);
+		}
+	}
+
+	getCategory();
+});
 
 $(".ad_section").slick({
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
+	infinite: true,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	fade: true,
+	speed: 500,
+	cssEase: "linear",
 });
